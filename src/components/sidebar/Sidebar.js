b57bbb98a4d6e252b4bca117fd3358b7e49075a2
ramon <pedroramon.r.lima@gmail.com>
2024-06-19 20:30:14 -0300

    Teste Menu

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,75 +2,74 @@ import React, { useState, useEffect } from "react";
 import { faHouse, faBook, faUser, faThumbsUp, faBars, faUserGroup, faGear, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import MenuItem from "../manuitem/MenuItem";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Logo from "../../assets/dev7.jpg"
+import Logo from "../../assets/dev7.jpg";
 
 const Sidebar = () => {
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
+  const [showSubmenu, setShowSubmenu] = useState(false);
 
-    const handlerCick = () => {
-        var menu = document.querySelector('.menu-opcoes-box');
-        if (menu.style.display === 'block' || menu.style.display === '') {
-            menu.style.display = 'none';
-            setShow(false);
-        } else {
-            menu.style.display = 'block';
-            setShow(true);
-        }
+  const handlerClick = () => {
+    var menu = document.querySelector('.menu-opcoes-box');
+    if (menu.style.display === 'block' || menu.style.display === '') {
+      menu.style.display = 'none';
+      setShow(false);
+    } else {
+      menu.style.display = 'block';
+      setShow(true);
+    }
+  };
+
+  const toggleSubmenu = () => {
+    setShowSubmenu(!showSubmenu);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      var menu = document.querySelector('.menu-opcoes-box');
+      if (window.innerWidth >= 768) {
+        setShow(true);
+        menu.style.display = 'block';
+      } else {
+        setShow(false);
+        menu.style.display = 'none';
+      }
     };
 
-    useEffect(() => {
-        // Função para atualizar o estado do menu com base no tamanho da janela
-        const handleResize = () => {
-            var menu = document.querySelector('.menu-opcoes-box');
-            if (window.innerWidth >= 768) {
-                setShow(true);
-                menu.style.display = 'block';
+    window.addEventListener('resize', handleResize);
 
-            } else {
-                setShow(false);
-                menu.style.display = 'none';
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
-            }
-        };
-
-        // Adiciona o event listener quando o componente é montado
-        window.addEventListener('resize', handleResize);
-
-        // Remove o event listener quando o componente é desmontado para evitar vazamentos de memória
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-    return (
-        <div className="menu-lateral-box">
-            <div className="logo">
-                <img
-                    src={Logo}
-                    alt="Logo"
-                />
-            </div>
-            <nav className="menu-opcoes-box">
-                <ul className="menu-opcoes-item menu-opcoes-list">
-                    <MenuItem icon={faHouse} text="Dashboard" route="/" />
-                    <MenuItem icon={faUser} text="Solicitar Usuário" route="/solicitacao-usuario" />
-                    <MenuItem icon={faBook} text="Requisições de Acesso" route="/requisicao-acesso" />
-                    <MenuItem icon={faThumbsUp} text="Aprovar/Reprovar" route="/aprovacao-reprovacao" />
-                    <a href="#">
-                        <li>
-                            <FontAwesomeIcon icon={faGear} /><span>Administração</span><FontAwesomeIcon icon={faChevronDown} />
-                            <ul>
-                                <MenuItem icon={faUserGroup} text="Grupos" route="/grupo-usuario" />
-                            </ul>
-                        </li>
-                    </a>
-                </ul>
-            </nav>
-            <button id="menu-toggle" className="menu-toggle" aria-label="Abrir menu" onClick={handlerCick}>
-                <FontAwesomeIcon icon={faBars} />
-            </button>
-        </div>
-    );
-}
+  return (
+    <div className="menu-lateral-box">
+      <div className="logo">
+        <img src={Logo} alt="Logo" />
+      </div>
+      <nav className="menu-opcoes-box">
+        <ul className="menu-opcoes-item menu-opcoes-list">
+          <MenuItem icon={faHouse} text="Dashboard" route="/" />
+          <MenuItem icon={faUser} text="Solicitar Usuário" route="/solicitacao-usuario" />
+          <MenuItem icon={faBook} text="Requisições de Acesso" route="/requisicao-acesso" />
+          <MenuItem icon={faThumbsUp} text="Aprovar/Reprovar" route="/aprovacao-reprovacao" />
+          <li>
+            <a href="#" onClick={toggleSubmenu}>
+              <FontAwesomeIcon icon={faGear} /><span>Administração</span><FontAwesomeIcon icon={faChevronDown} />
+            </a>
+            {showSubmenu && (
+              <ul className="submenu">
+                <MenuItem icon={faUserGroup} text="Grupos" route="/grupo-usuario" />
+              </ul>
+            )}
+          </li>
+        </ul>
+      </nav>
+      <button id="menu-toggle" className="menu-toggle" aria-label="Abrir menu" onClick={handlerClick}>
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+    </div>
+  );
+};
 
 export default Sidebar;
