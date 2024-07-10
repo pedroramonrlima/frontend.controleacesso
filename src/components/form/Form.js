633385cca1b05674a4156e89be13877a5b88c5b4
ramon <pedroramon.r.lima@gmail.com>
2024-07-10 00:06:46 -0300

    Form GroupADD

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,27 +1,43 @@
 import React, { useState } from "react";
-import "./form.css"
+import "./form.css";
 
 const Form = ({ children }) => {
+    const [requiresApproval, setRequiresApproval] = useState(false);
 
+    const handleApprovalChange = (event) => {
+        setRequiresApproval(event.target.checked);
+    };
 
     return (
         <>
             <form className="form-box-2">
-  
                 <div className="input-field">
-                    <label for="input1">Nome:</label>
+                    <label htmlFor="input1">Nome:</label>
                     <input type="text" id="input1" required placeholder=" " />
                 </div>
                 <div className="input-field">
-                    <label for="input2">DN Grupo ActiveDirectory:</label>
+                    <label htmlFor="input2">DN Grupo ActiveDirectory:</label>
                     <input type="text" id="input2" required placeholder=" " />
-                </div>           
-            <div className="btn-box center">
-                <button>Cadastrar</button>
-            </div>
+                </div>
+                <div className="input-field">
+                    <label className="switch">
+                        <input
+                            type="checkbox"
+                            checked={requiresApproval}
+                            onChange={handleApprovalChange}
+                        />
+                        <span className="slider round"></span>
+                    </label>
+                    <label htmlFor="approvalSwitch" className="switch-label">
+                        Requer aprovação de especialista ?
+                    </label>
+                </div>
+                <div className="btn-box center">
+                    <button>Cadastrar</button>
+                </div>
             </form>
         </>
     );
-}
+};
 
-export default Form
+export default Form;
